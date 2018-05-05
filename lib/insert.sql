1cb6f8b2e4fe84dc0ddd03998e278a1bde8b7ff3
Karen G <kgraves0701@gmail.com>
2018-05-05 12:45:45 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,25 +8,22 @@ INSERT INTO books VALUES (4, "Fourth Knot", 2, 2002);
 INSERT INTO books VALUES (5, "Fifth Elephant", 1, 2007);
 INSERT INTO books VALUES (6, "Sixth Sensed", 2, 2017);
 
--- INSERT INTO characters VALUES (1, "It Begins", 1, 2018);
+INSERT INTO characters VALUES (1, "Prime", "betazoid", "feel your feelings", 1, 1);
+INSERT INTO characters VALUES (2, "Moss", "human", "oh no", 2, 2);
+INSERT INTO characters VALUES (3, "Roy", "human", "sea parks", 1, 2);
+INSERT INTO characters VALUES (4, "Jen", "klingon", "the internet", 2, 1);
+INSERT INTO characters VALUES (5, "Emo", "romulan", "goth to boss", 1, 1);
+INSERT INTO characters VALUES (6, "David", "posh", "that did not happen", 1, 2);
+INSERT INTO characters VALUES (7, "Lee", "mentiroso", "i swear it did", 2, 1);
+INSERT INTO characters VALUES (8, "Welshie", "dalek", "lucky charms", 2, 2);
 
--- 
--- CREATE TABLE characters (
---   id INTEGER PRIMARY KEY,
---   name TEXT,
---   species TEXT,
---   motto TEXT,
---   series_id INTEGER,
---   author_id INTEGER);
---   
--- CREATE TABLE subgenres (
---   id INTEGER PRIMARY KEY,
---   name TEXT);
--- 
--- CREATE TABLE authors (
---   id INTEGER PRIMARY KEY,
---   name TEXT);
--- 
+INSERT INTO subgenres VALUES (1, "Androids");
+INSERT INTO subgenres VALUES (2, "AI");
+
+INSERT INTO authors VALUES (1, "Alpha");
+INSERT INTO authors VALUES (2, "Omega");
+
+
 -- 
 -- CREATE TABLE character_books (
 --   id INTEGER PRIMARY KEY,
