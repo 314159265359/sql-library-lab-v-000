a8c7f780a02c66adbb3dea25533edc3a999f5437
Karen G <kgraves0701@gmail.com>
2018-05-05 12:36:57 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,35 @@
+INSERT INTO series VALUES (1, "Robots", 1, 1);
+INSERT INTO series VALUES (2, "Aliens", 2, 2);
+
+INSERT INTO books VALUES (1, "It Begins", 1, 2018);
+INSERT INTO books VALUES (2, "Seconds", 2, 2000);
+INSERT INTO books VALUES (3, "Third Wave", 1, 2009);
+INSERT INTO books VALUES (4, "Fourth Knot", 2, 2002);
+INSERT INTO books VALUES (5, "Fifth Elephant", 1, 2007);
+INSERT INTO books VALUES (6, "Sixth Sensed", 2, 2017);
+
+-- INSERT INTO characters VALUES (1, "It Begins", 1, 2018);
+
+-- 
+-- CREATE TABLE characters (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   species TEXT,
+--   motto TEXT,
+--   series_id INTEGER,
+--   author_id INTEGER);
+--   
+-- CREATE TABLE subgenres (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT);
+-- 
+-- CREATE TABLE authors (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT);
+-- 
+-- 
+-- CREATE TABLE character_books (
+--   id INTEGER PRIMARY KEY,
+--   book_id INTEGER,
+--   character_id INTEGER);
+
